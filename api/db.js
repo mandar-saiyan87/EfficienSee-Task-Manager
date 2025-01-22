733f36db4a95a90041182d66a917cc464acf20cd
mandar-saiyan87 <mdman2257@gmail.com>
2025-01-22 21:54:21 +0530

    user register api done

--- api/db.js
+++ api/db.js
@@ -3,9 +3,9 @@ import { config } from "dotenv";
 
 // Dontenv config
 config();
-let { MONGODB_PASSWD } = process.env;
+const { MONGODB_PASSWD } = process.env;
 // MongoDB URI
-const MongoDBURI = `mongodb+srv://mdman2257:${MONGODB_PASSWD}@cluster0.w3vvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const MongoDBURI = `mongodb+srv://mdman2257:${MONGODB_PASSWD}@cluster0.w3vvt.mongodb.net/afftask?retryWrites=true&w=majority&appName=Cluster0`;
 
 export async function connectDB() {
   // Connection to MongoDB
