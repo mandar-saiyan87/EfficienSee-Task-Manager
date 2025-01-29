1aa0d29787b5d85d0159463edc2d30c3770960a2
mandar-saiyan87 <mdman2257@gmail.com>
2025-01-29 18:29:18 +0530

    deleted unwanted files

--- backend/efftask/settings.py
+++ backend/efftask/settings.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api',
     'users',
+    'taskapp',
 ]
 
 AUTH_USER_MODEL = 'users.User'
